6bdb2932f794fbaed31fc4b3dbfbcd9d8bf8148a
inori-lover <337938188@qq.com>
2020-08-06 18:47:05 +0800

    feat: basic interact

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Guess the number!");
+  println!("please input your guess.");
+
+  let mut guess = String::new();
+
+  io::stdin()
+    .read_line(&mut guess)
+    .expect("fail to read line");
+
+  println!("your guess is {}", guess);
 }
